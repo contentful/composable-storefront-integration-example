7ebe57219fa93671dce5adcaafbc5d26463dfd0e
Michael Reynolds <mike.reynolds@contentful.com>
2025-10-28 14:50:36 -0400

    minor configuration updates to support different environments

--- src/app/contentful/root/config/default-contentful-config.ts
+++ src/app/contentful/root/config/default-contentful-config.ts
@@ -1,12 +1,13 @@
+import { environment } from '../../../../environments/environment';
 import { ContentfulConfig } from './contentful-config';
 
 export const defaultContentfulConfig: ContentfulConfig = {
   contentful: {
-    spaceId: 'YOUR_SPACE_ID',
-    accessToken: 'YOUR_DELIVERY_API_TOKEN',
-    previewAccessToken: 'YOUR_PREVIEW_API_TOKEN',
-    environment: 'master',
-    deliveryApiUrl: 'cdn.contentful.com',
-    previewApiUrl: 'preview.contentful.com',
+    spaceId: environment.contentful.spaceId,
+    accessToken: environment.contentful.accessToken,
+    previewAccessToken: environment.contentful.previewAccessToken,
+    environment: environment.contentful.environment,
+    deliveryApiUrl: environment.contentful.deliveryApiUrl,
+    previewApiUrl: environment.contentful.previewApiUrl,
   },
 };
